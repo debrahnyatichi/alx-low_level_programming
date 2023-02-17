9de0d09611c77cf0e48581467e1704339c03421e
debrahnyatichi <binyanyadebby@gmail.com>
2023-02-17 11:21:07 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,14 +18,12 @@ int main(void)
 			putchar((digit2 % 10) + '0');
 
 			if (digit1 == 8 && digit2 == 9)
-				continue;
-			
+				continue;	
 			putchar(',');
 			putchar(' ');
 		}
 	}
 
 	putchar('\n');
-
 	return (0);
 }
