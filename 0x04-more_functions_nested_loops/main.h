bf00ae058475355967ceca117fb67a8a0a216d5b
debrahnyatichi <binyanyadebby@gmail.com>
2023-02-23 05:31:50 -0800

    0-isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -13,4 +13,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-#endif
+
+#endif /* MAIN_H */
