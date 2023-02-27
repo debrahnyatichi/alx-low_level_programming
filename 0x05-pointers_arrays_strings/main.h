656424fcab75e49c8206af40ba4e041ecb2ddcb4
debrahnyatichi <binyanyadebby@gmail.com>
2023-02-27 11:00:02 -0800

    changes on my files and 3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
 void reset_to_98(int *n);
+int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -8,4 +11,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+#endif
