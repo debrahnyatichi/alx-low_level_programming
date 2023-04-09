60da694771542ee7f770067672b17402a4bd884a
debrahnyatichi <binyanyadebby@gmail.com>
2023-04-09 12:35:29 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,9 +11,9 @@ void print_binary(unsigned long int n)
 	if (n >> 0)
 	{
 		if (n >> 1)
-			print_binary(j >> 1);
-		_putchar((j & 1) + '0');
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
 	else
-		_putchar (0);
+		_putchar ('0');
 }
