ed30af86202a91233c00a4568eb9a0cc1c4033ff
debrahnyatichi <binyanyadebby@gmail.com>
2023-06-09 06:33:08 -0700

    deleted my files

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
